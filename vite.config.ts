0364ec1eb0454991b91fcba20e9c5db04c0382cf
Jesus Flores <jesusraflores1202@gmail.com>
2026-04-18 19:37:46 -0400

    Second commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jesusrflores/movie-finder-app',
+  base: '/movie-finder-app',
 });
